ce9b7e4dabb4dbbc23d7de721e5f8f1d8e98a1bc
Jellorine <jjello1356@gmail.com>
2025-05-04 21:46:45 +0530

    getters setters added

--- backend/demo/src/main/java/com/example/demo/model/Product.java
+++ backend/demo/src/main/java/com/example/demo/model/Product.java
@@ -22,4 +22,66 @@ public class Product {
     private int quantityInStock;
     private String unit;
     private Boolean status;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBarcode() {
+		return barcode;
+	}
+	public void setBarcode(String barcode) {
+		this.barcode = barcode;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public double getCostPrice() {
+		return costPrice;
+	}
+	public void setCostPrice(double costPrice) {
+		this.costPrice = costPrice;
+	}
+	public int getQuantityInStock() {
+		return quantityInStock;
+	}
+	public void setQuantityInStock(int quantityInStock) {
+		this.quantityInStock = quantityInStock;
+	}
+	public String getUnit() {
+		return unit;
+	}
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+	public Boolean getStatus() {
+		return status;
+	}
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+    
+    
 }
